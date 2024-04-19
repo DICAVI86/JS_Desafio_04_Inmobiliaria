13c052003a18972493e2e340ad3f12e8494c532b
DICAVI86 <delcastillovial@gmail.com>
2024-04-19 17:44:01 -0400

    Se esta trabajando sobre el Script de la ventana de Alquileres

---
+++ assets/js/script_venta.js
@@ -0,0 +1,64 @@
+// Arreglo Venta
+
+const propiedades_venta = [
+    {
+    nombre: 'Apartamento de lujo en zona exclusiva',
+    src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+    descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    habitaciones: 4,
+    costo: 5.000,
+    smoke: false,
+    pets: true
+    },
+    {
+    nombre: '02',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    },
+    {
+    nombre: '03',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    },
+    {
+    nombre: '04',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    },
+    {
+    nombre: '05',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    },
+    {
+    nombre: '06',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    }
+    ]
